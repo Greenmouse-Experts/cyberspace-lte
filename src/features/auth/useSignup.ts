a3121b036f1444d48e9367db1c07972e72201319
iammoses <inawo.moses@gmail.com>
2024-05-09 14:01:01 +0100

    error added

--- src/features/auth/useSignup.ts
+++ src/features/auth/useSignup.ts
@@ -1,12 +1,22 @@
 import { useMutation } from "@tanstack/react-query";
 import { signup as SignUpApi } from "../../services/authApi";
 import toast from "react-hot-toast";
+interface NetworkError {
+  response: {
+    data: {
+      message: string;
+    };
+  };
+}
 
 export function useSignup() {
   const { mutate: signup, isLoading } = useMutation({
     mutationFn: SignUpApi,
     onSuccess: () => {
       toast.success("Account successfully created! Please verify your account ");
+    },
+    onError: (error:NetworkError) => {
+      toast.error(`${error.response.data.message}`);
     },
   });
 
