64d8988b3d45b46b37e7dc7e19a0abf42c4a13b6
iammoses <inawo.moses@gmail.com>
2024-05-27 16:59:37 +0100

    adjusted

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -16,7 +16,7 @@ function About() {
             of ISPs. Our network is designed for the ultimate performance,
             delivering the speed you need to stay ahead of the curve.
           </p>
-          <p className="mt-3">
+          <p className="mt-5">
             For the Nigerian youth carving their path, for the professionals
             shaping the future, and for the businesses setting the trends,
             Superfast is the name you can trust. With us, you’re not just
