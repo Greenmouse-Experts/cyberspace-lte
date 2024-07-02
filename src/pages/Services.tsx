1c23bf0165eab6d1e54b9304c76a9038df14aaac
iammoses <inawo.moses@gmail.com>
2024-07-02 14:44:08 +0100

    updated

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -7,7 +7,7 @@ function Services() {
       <section className="pt-20 grid xl:grid-cols-3 gap-x-32">
        <div className="flex flex-col gap-10 col-span-2">
        <div className=" pt-10 bg-white dark:bg-black rounded-[10px] pb-8 px-8 cardShadow">
-          <h4 className="text-center mb-3">Residential Internet</h4>
+          <h4 className="text-start mb-3">Residential Internet</h4>
           <p className="font-normal">
             We offer high-speed broadband internet packages tailored to meet the
             needs of modern households. Whether streaming, gaming, or browsing,
@@ -16,7 +16,7 @@ function Services() {
           </p>
         </div>
         <div className="pt-10 bg-white dark:bg-black rounded-[10px] pb-8 px-8 cardShadow ">
-          <h4 className="text-center mb-3">Small Medium Enterprises (SMEs).</h4>
+          <h4 className="text-start mb-3">Small Medium Enterprises (SMEs).</h4>
           <p className="font-normal">
             Our business internet solutions are designed to fuel productivity
             and growth for small businesses. With dedicated support and
@@ -25,7 +25,7 @@ function Services() {
           </p>
         </div>
         <div className="pt-10 bg-white dark:bg-black rounded-[10px] pb-8 px-8 cardShadow ">
-          <h4 className="text-center mb-3">IP Voice</h4>
+          <h4 className="text-start mb-3">IP Voice</h4>
           <p className="font-normal">
             Our managed services ensure hassle-free internet experiences for our
             clients. From installation and maintenance to troubleshooting and
